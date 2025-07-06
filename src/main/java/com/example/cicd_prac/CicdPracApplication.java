82ab910ada964f1852813ed1e5e47ed3c0cb610a
김도훈 <dohoon@hoonis-MacBook-Pro.local>
2025-07-07 00:17:16 +0900

    fist-commit

---
+++ src/main/java/com/example/cicd_prac/CicdPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPracApplication.class, args);
+	}
+
+}
